981a66d163c874cd1d699ce9b1383dc54b0f8d0a
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-04-12 16:58:18 +1000

    Separate credentials and add .gitignore file.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from credentials import USER_NAME, PASSWORD
+
 LOGIN_URL = 'https://login.live.com/'
 REWARD_URL = 'https://rewards.bing.com/'
-EMAIL = "MY_EMAIL"
-PASSWD = "MY_PASSWORD"
 
 EMAILFIELD = (By.ID, "i0116")
 PASSWORD_FIELD = (By.ID, "i0118")
@@ -20,13 +20,13 @@ browser = webdriver.Chrome()
 browser.get(REWARD_URL)
 
 # wait for email field and enter email
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys(EMAIL)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys(USER_NAME)
 
 # Click Next
 WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXT_BUTTON)).click()
 
 # wait for password field and enter password
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(PASSWORD_FIELD)).send_keys(PASSWD)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(PASSWORD_FIELD)).send_keys(PASSWORD)
 
 # Click Login - same id?
 WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXT_BUTTON)).click()
