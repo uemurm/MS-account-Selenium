9fa8557a0033b7e16395955360fc2d32e1f437a9
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-04-25 13:22:26 +1000

    Navigate to Bing URL then enter search key words.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from credentials import USER_NAME, PASSWORD
+from search_words import search_words
 
 LOGIN_URL = 'https://login.live.com/'
 REWARD_URL = 'https://rewards.bing.com/'
@@ -16,8 +17,10 @@ PASSWORD_FIELD = (By.ID, "i0118")
 NEXT_BUTTON = (By.ID, "idSIButton9")
 DECLINE_BUTTON = (By.ID, "declineButton")
 
+# Edge is used with my own profile so let's use Chrome here.
 browser = webdriver.Chrome()
-browser.get(REWARD_URL)
+browser.maximize_window()
+browser.get(LOGIN_URL)
 
 # wait for email field and enter email
 WebDriverWait(browser, 10).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys(USER_NAME)
@@ -33,3 +36,18 @@ WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXT_BUTTON)).click(
 
 # Stay signed in?
 WebDriverWait(browser, 10).until(EC.element_to_be_clickable(DECLINE_BUTTON)).click()
+
+
+BING_URL = 'https://www.bing.com/'
+SEARCH_BOX = (By.ID, "sb_form_q")
+FIRST_NAME = (By.ID, "id_n")
+
+browser.get(BING_URL)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(FIRST_NAME))
+
+for word in search_words:
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(SEARCH_BOX)).send_keys(word + "\n")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(SEARCH_BOX)).clear()
+
+while True:
+    pass
---
+++ search_words.py
@@ -0,0 +1,30 @@
+'''
+Module to define search words.
+'''
+search_words = {
+    'Beef',
+    'Burger'
+    # 'Nelson',
+    # 'Journey',
+    # 'Japan',
+    # 'Australia',
+
+    # 'baseball',
+    # 'football',
+    # 'soccer',
+    # 'tennis',
+
+    # 'Chopin',
+    # 'Bach',
+    # 'Beethoven',
+
+    # 'イー',
+    # 'リャン',
+    # 'サン',
+    # 'スー',
+    # 'ウー',
+    # 'ロー',
+    # 'チー',
+    # 'パー',
+    # 'キュー',
+}
